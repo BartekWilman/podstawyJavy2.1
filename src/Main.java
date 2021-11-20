7fd3d84a5a50c1a75863187c8991a85100b5aee8
Bartłomiej Wilman <bartekwilman@op.pl>
2021-11-20 10:40:06 +0100

    pętla for (cars)

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,12 @@
 public class Main {
+    public static void main(String[] args) {
+        int [] tablica = {1,2,3,4,5,6};
+        for(int i:tablica){
+            System.out.println(tablica);
+        }
+//      String [] cars = {"toyota", "opel", "mazda", "bmw"};
+//      for(String car:cars){
+//          System.out.println(car);
+//      }
+    }
 }
